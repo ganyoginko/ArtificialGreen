bcafee97198151d3496f8b20663d629518bb6e27
小松海渡 <kk@komatsukaidonoMacBook-Air.local>
2020-10-18 02:00:02 +0900

    Day:.

---
+++ main.js
@@ -0,0 +1,37 @@
+const  {exec}  = require('child_process')
+const fs = require('fs');
+const cron = require('node-cron');
+
+let dayCnt = 0;
+
+cron.schedule('0 0 2 * * *', () => {
+    dayCnt++;
+    fs.appendFile('test.js', `//Day ${dayCnt}`, (err) => {
+	if (err)
+	    throw err;
+	else{
+	    exec('git add . ', (error,stdout,stderr) =>{
+		if(error){
+		    console.log(`Day${dayCnt}: ${stderr}`);
+		    return;
+		}else{
+		    exec('git commit -m Day:${dayCnt}. ', (error,stdout,stderr) =>{
+			if(error){
+			    console.log(`Day${dayCnt}:error: ${stderr}`);
+			    return;
+			}else{
+		            exec('git push origin master . ', (error,stdout,stderr) =>{
+				if(error){
+				    console.log(`Day${dayCnt}:error: ${stderr}`);
+				    return;
+				}else{
+				    console.log(`Day${dayCnt}:success: You did it also today.  Good Job.`);
+				}
+			    })
+			}
+		    })
+		}
+	    })
+	}
+    });
+});
