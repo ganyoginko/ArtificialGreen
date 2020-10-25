072a7da99af616f527c0677d3f687b5436eb28b1
小松海渡 <kk@komatsukaidonoMacBook-Air.local>
2020-10-25 13:43:16 +0900

    Day:

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let dayCnt = 2;
 
 cron.schedule('* * * * * *', () => {
     dayCnt++;
-    fs.appendFile('main.js', `//Day ${dayCnt}`, (err) => {
+    fs.appendFile('main.js', `//Day ${dayCnt}\n`, (err) => {
 	if (err)
 	    throw err;
 	else{
@@ -37,3 +37,4 @@ cron.schedule('* * * * * *', () => {
 });
 //Day 2
 //Day 3//Day 4//Day 5//Day 6//Day 7//Day 8//Day 9//Day 10//Day 11
+//Day 3
